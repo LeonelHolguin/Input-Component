943be7c0f6f9b9fe6d9baee5a2b1e17cd49adec1
LeonelHolguin <Leonelinf16@gmail.com>
2023-02-08 00:09:01 -0400

    add 4 attribute of the input component and create label, section and p elements

--- Input-constants.js
+++ Input-constants.js
@@ -1,4 +1,7 @@
+
 export const inputCSS = `
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+
 /* fallback */
 @font-face {
     font-family: 'Material Icons';
@@ -22,4 +25,74 @@ export const inputCSS = `
     -webkit-font-feature-settings: 'liga';
     -webkit-font-smoothing: antialiased;
   }
+
+  .component-container {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+
+  p {
+    display: none;
+    font-family: 'Noto Sans JP';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 14px;
+    color: #828282;
+    margin-top: 4px;
+  }
+
+  label {
+    font-family: 'Noto Sans JP';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 17px;
+    color: #333333;
+    margin-bottom: 4px;
+  }
+
+  input {
+    font-family: 'Noto Sans JP';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+    outline: none;
+    border: 1px solid #828282;
+    border-radius: 8px;
+    width: 200px;
+    height: 56px;
+    box-sizing: border-box;
+  }
+  
+  :host(:focus-within) label{
+    color: #2962FF;
+  }
+
+  :host(:focus-within) input {
+    border-color: #2962FF;
+  }
+
+  label:hover, label:active, input:hover+label, input:active+label, input:hover {
+    color: #333333;
+    border-color: #333333;
+  }
+
+  .labelError {
+    color: #D32F2F;
+  }
+
+  .inputError {
+    border-color: #D32F2F;
+  }
+
+  :host(:focus-within) .labelError, .pError {
+    color: #D32F2F;
+  }
+  
+  :host(:focus-within) .inputError {
+    border-color: #D32F2F;
+  }
+  
 `
--- Input.js
+++ Input.js
@@ -8,16 +8,77 @@ export class Input extends HTMLElement {
 
         this.inputElement = document.createElement("input");
         this.shadowDOM.appendChild(this.inputElement);
+        
+        this.labelElement = document.createElement("label");
+        this.shadowDOM.appendChild(this.labelElement);
+
+        this.pElement = document.createElement("p");
+        this.shadowDOM.appendChild(this.pElement);
+
+        this.sectionElement = document.createElement("section");
+        this.shadowDOM.appendChild(this.sectionElement);
+
+        this.sectionElement.appendChild(this.pElement);
+        this.sectionElement.appendChild(this.inputElement);
+        this.sectionElement.appendChild(this.labelElement);
     }
 
-    connectedCallBack() {
+    connectedCallback() {
+        this.labelElement.appendChild(
+            document.createTextNode(this.getAttribute("label") ?? "Label")
+          );
+
+          this.sectionElement.setAttribute("class", "component-container");
+
         let inputStyle = document.createElement("style");
         inputStyle.innerHTML = inputCSS;
-
-        if (this.hasAttribute("class"))
-            this.inputElement.setAttribute("class", this.getAttribute("class"));
-          
-
         this.shadowDOM.appendChild(inputStyle);
+
+        this.setName();
+        this.setPlaceholder();
+        this.setErrorState();
+        this.setDisabled();
+        this.setHelperText();
     }
+
+    setName(){
+        let name = this.getAttribute("name") || "nameDefault";
+
+        this.labelElement.setAttribute("for", name);
+        this.inputElement.setAttribute("id", name);
+    }
+
+    setPlaceholder() {
+        let placeholderValue = this.getAttribute("placeholder") || "Placeholder";
+        this.inputElement.placeholder = placeholderValue;
+    }
+
+    setErrorState() {
+        if(!this.hasAttribute("error")) return;
+
+        this.inputElement.setAttribute("class", "inputError");
+        this.labelElement.setAttribute("class", "labelError");
+    }
+
+    setDisabled() {
+        if(!this.hasAttribute("disabled")) return
+
+        this.inputElement.disabled = true;
+        this.inputElement.style.borderColor = "#E0E0E0";
+        this.inputElement.style.backgroundColor = "#F2F2F2";
+    }
+
+    setHelperText() {
+        if(!this.hasAttribute("helpertext")) return; 
+        
+        this.pElement.appendChild(
+            document.createTextNode(this.getAttribute("helpertext") || "Empty")
+          );
+        this.pElement.style.display = "block";
+
+        if(!this.hasAttribute("error")) return;
+
+        this.pElement.setAttribute("class", "pError");
+    }
+
 }
